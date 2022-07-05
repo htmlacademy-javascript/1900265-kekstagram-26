6655570e838ed70ead3efdbd417ba4373f1da76e
Keks <keks@htmlacademy.ru>
2022-07-05 16:32:17 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/form-upload.js
+++ 7/js/form-upload.js
@@ -1,56 +1,16 @@
-/* Форма редактирования изображения
-Выбор изображения для загрузки осуществляется с помощью стандартного контрола загрузки файла #upload-file, который стилизован под букву «О» в логотипе.
-
-1. Показать форму:
-  - После выбора изображения (изменения значения поля #upload-file), показывается форма редактирования изображения.
-  - У элемента .img-upload__overlay удаляется класс hidden.
-  - У элемента body задаётся класс modal-open.
-
-2. Закрыть форму:
-  - производится либо нажатием на кнопку #upload-cancel, либо нажатием клавиши Esc.
-  - Элементу .img-upload__overlay возвращается класс hidden.
-  - У элемента body удаляется класс modal-open.
-
-3. Валидация хештегов
-Набор хэш-тегов можно превратить в массив, воспользовавшись методом .split().
-Написать цикл, который будет ходить по полученному массиву и проверять каждый из хэш-тегов на предмет соответствия ограничениям.
-Если хотя бы один из тегов не проходит нужных проверок, показывать сообщение об ошибке.
-ТЗ 2.3. Хэш-теги:
-- хэш-тег начинается с символа # (решётка);
-- строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.;
-- хеш-тег не может состоять только из одной решётки;
-- максимальная длина одного хэш-тега 20 символов, включая решётку;
-- хэш-теги нечувствительны к регистру: #ХэшТег и #хэштег считаются одним и тем же тегом;
-- хэш-теги разделяются пробелами;
-- один и тот же хэш-тег не может быть использован дважды;
-- нельзя указать больше пяти хэш-тегов;
-- хэш-теги необязательны;
-- если фокус находится в поле ввода хэш-тега, нажатие на Esc не должно приводить к закрытию формы редактирования изображения.
-
-4. Валидация комментариев:
-ТЗ  2.4. Комментарий:
-- комментарий не обязателен;
-- длина комментария не может составлять больше 140 символов;
-- если фокус находится в поле ввода комментария, нажатие на Esc не должно приводить к закрытию формы редактирования изображения.
-*/
 import {isEscapeKey} from './utils.js';
 
 const uploadForm = document.querySelector('.img-upload__form');
 const uploadFile = uploadForm.querySelector('#upload-file');
 const bodyElement = document.querySelector('body');
 const uploadCancel = uploadForm.querySelector('#upload-cancel');
-
-uploadFile.addEventListener('change', () => {
-  document.addEventListener('keydown', keydownEscapeHandler);
-  uploadCancel.addEventListener('click', clickHandler);
-  uploadForm.querySelector('.img-upload__overlay').classList.remove('hidden');
-  bodyElement.classList.add('modal-open');
-});
-
 const textHashtagsElement = uploadForm.querySelector('.text__hashtags');
 const textDescriptionElement = uploadForm.querySelector('.text__description');
+const templateSuccess = document.querySelector('#success').content.querySelector('.success');
+const templateError = document.querySelector('#error').content.querySelector('.error');
+const uploadTextElement = document.querySelector('.img-upload__text');
 
-function keydownEscapeHandler(evt) {
+const keydownEscapeHandler = (evt) => {
   if (isEscapeKey(evt)) {
     if (document.activeElement !== textHashtagsElement && document.activeElement !== textDescriptionElement) {
       evt.preventDefault();
@@ -59,34 +19,37 @@ function keydownEscapeHandler(evt) {
       document.removeEventListener('keydown', keydownEscapeHandler);
     }
   }
-}
+};
 
-function clickHandler() {
+const clickHandler = () => {
   uploadForm.querySelector('.img-upload__overlay').classList.add('hidden');
   bodyElement.classList.remove('modal-open');
   uploadCancel.removeEventListener('click', clickHandler);
-}
+};
 
-const pristine = new Pristine(uploadForm);
-const templateSuccess = document.querySelector('#success').content.querySelector('.success');
-const templateError = document.querySelector('#error').content.querySelector('.error');
-const uploadTextElement = document.querySelector('.img-upload__text');
+uploadFile.addEventListener('change', () => {
+  document.addEventListener('keydown', keydownEscapeHandler);
+  uploadCancel.addEventListener('click', clickHandler);
+  uploadForm.querySelector('.img-upload__overlay').classList.remove('hidden');
+  bodyElement.classList.add('modal-open');
+});
 
-const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+const pristine = new Pristine(uploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__error-text'
+});
 
-function hashtagValidator (value) {
+const HASHTAG_RE = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+const MAX_RENDER_HASHTAGS = 5;
+
+const validateHashtag = (value) => {
   const words = value.trim().split(' ');
-  let isValid = words.every((word) => re.test(word));
-  if (isValid) {
-    isValid = new Set(words).size === words.length;
-  }
-  if (isValid) {
-    isValid = words.length <= 5;
-  }
+  const isValid = value === '' || words.every((word) => HASHTAG_RE.test(word)) && new Set(words).size === words.length && words.length <= MAX_RENDER_HASHTAGS;
   return isValid;
-}
+};
 
-pristine.addValidator(textHashtagsElement, hashtagValidator);
+pristine.addValidator(textHashtagsElement, validateHashtag, 'ХэшТэг начинается с # и содержит не более 20 любых букв и цифр. Можно использовать не более 5 неповторяющихся ХэшТэгов.');
 
 uploadForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
