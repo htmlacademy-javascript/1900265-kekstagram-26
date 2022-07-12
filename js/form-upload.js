6371b3ccd3649b50729c130b3785c58e80891e8f
AlexEgorova <alex.egorova.msk@gmail.com>
2022-07-12 12:39:05 +0300

    Помощь друга

--- js/form-upload.js
+++ js/form-upload.js
@@ -117,3 +117,92 @@ uploadForm.addEventListener('submit', (evt) => {
 
 //слайдер
 
+/*
+2.2. Наложение эффекта на изображение:
+1/ По умолчанию должен быть выбран эффект «Оригинал».
+
+2/  На изображение может накладываться только один эффект.
+
+3/  При смене эффекта, выбором одного из значений среди радиокнопок .effects__radio,
+добавить картинке внутри .img-upload__preview CSS-класс, соответствующий эффекту.
+Например, если выбран эффект .effect-chrome, изображению нужно добавить
+класс effects__preview--chrome.
+
+4/  Интенсивность эффекта регулируется перемещением ползунка в слайдере.
+Слайдер реализуется сторонней библиотекой для реализации слайдеров noUiSlider.
+Уровень эффекта записывается в поле .effect-level__value.
+ри изменении уровня интенсивности эффекта (предоставляется API слайдера),
+CSS-стили картинки внутри .img-upload__preview обновляются следующим образом:
+  - Для эффекта «Хром» — filter: grayscale(0..1) с шагом 0.1;
+  - Для эффекта «Сепия» — filter: sepia(0..1) с шагом 0.1;
+  - Для эффекта «Марвин» — filter: invert(0..100%) с шагом 1%;
+  - Для эффекта «Фобос» — filter: blur(0..3px) с шагом 0.1px;
+  - Для эффекта «Зной» — filter: brightness(1..3) с шагом 0.1;
+  - Для эффекта «Оригинал» CSS-стили filter удаляются.
+
+5/ При выборе эффекта «Оригинал» слайдер скрывается.
+
+6/ При переключении эффектов, уровень насыщенности сбрасывается до начального значения (100%):
+слайдер, CSS-стиль изображения и значение поля должны обновляться.
+
+7/  Кроме визуального применения эффекта необходимо записывать значение
+в скрытое поле для дальнейшей отправки на сервер.
+
+8/ при переключении фильтра, уровень эффекта должен сразу сбрасываться до начального состояния,
+т. е. логика по определению уровня насыщенности должна срабатывать не только при «перемещении» слайдера,
+но и при переключении фильтров.
+*/
+
+const sliderElement = document.querySelector('.effect-level__slider');
+const effectsRadioList = document.querySelectorAll('.effects__radio');
+const effectsListElement = document.querySelector('.effects__list');
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 1,
+  },
+  start: 1,
+  step: 0.1,
+  connect: 'lower',
+});
+
+const getCurrentEffect = () => {
+  let effect;
+  effectsRadioList.forEach((item) => {
+    if (item.checked === true) {
+      effect = item.value;
+    }
+  });
+  return effect;
+};
+
+const resetAllEffects = () => {
+  imgUploadPreviewElement.classList.forEach((item) => {
+    if (item.startsWith('effects__preview--')) {
+      imgUploadPreviewElement.classList.remove(item);
+    }
+  });
+};
+
+const EFFECT_ORIGINAL = 'none';
+
+const setupEffect = (effect) => {
+  if (effect !== EFFECT_ORIGINAL) {
+    imgUploadPreviewElement.classList.add(`effects__preview--${effect}`);
+  }
+};
+
+const applyEffect = () => {
+  const currentEffect = getCurrentEffect();
+  resetAllEffects();
+  setupEffect(currentEffect);
+};
+
+effectsListElement.addEventListener('click', (evt) => {
+  if (evt.target.nodeName === 'INPUT') {
+    applyEffect();
+  }
+});
+
+
